b05fce1c9f60fa4065e992b29054321d5d142774
Esther Kim <kim.esther117@gmail.com>
2022-12-06 19:09:15 -0800

    Feature 16 Complete

--- tasks/views.py
+++ tasks/views.py
@@ -19,3 +19,11 @@ def create_task(request):
         "form": form,
     }
     return render(request, "tasks/create.html", context)
+
+@login_required
+def show_my_tasks(request):
+    task_list = Task.objects.filter(assignee=request.user)
+    context = {
+        "task_list": task_list
+    }
+    return render(request, "tasks/list.html", context)
