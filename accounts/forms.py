db574aa26f46990258cfce934e1a9863f49ace53
Esther Kim <kim.esther117@gmail.com>
2022-12-06 17:38:26 -0800

    Feature 10 Complete

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,10 +1,19 @@
 from django import forms
-from django.forms import ModelForm
-
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=150)
     password = forms.CharField(
+        max_length=150,
+        widget=forms.PasswordInput,
+    )
+
+class SignUpForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(
+        max_length=150,
+        widget=forms.PasswordInput,
+    )
+    password_confirmation = forms.CharField(
         max_length=150,
         widget=forms.PasswordInput,
     )
